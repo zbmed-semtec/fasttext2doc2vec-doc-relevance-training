8ae800ff0ef7e40cb6029f6d4c5093c143b0d827
rohitharavinder <rohitha0112@gmail.com>
2024-08-01 08:49:00 +0000

    🌱 add yaml file for hyperparameter configurations

--- code/train.py
+++ code/train.py
@@ -4,7 +4,7 @@ import argparse
 import logging
 import utilities as utilities
 
-def run(best_params, args, tuning=False, save_model=False):
+def run(best_params, args, save_model=False):
 
     # 1) Load the training data
     train_pmids, train_docs = utilities.process_data_from_npy(args.input)
@@ -18,15 +18,10 @@ def run(best_params, args, tuning=False, save_model=False):
     logging.info("RELISH fastText Model Generated")
     logging.info("Model is being used.")
 
-    # 3) Set the validation/test data to be used based on tuning parameter
-    if tuning:
-        dataset_type = "Validation"
-        data_file = args.valid
-        ground_truth = args.valid_ground_truth
-    else:
-        dataset_type = "Test"
-        data_file = args.test
-        ground_truth = args.test_ground_truth
+    # 3) Set the test data to be used based on tuning parameter
+    dataset_type = "Test"
+    data_file = args.test
+    ground_truth = args.test_ground_truth
 
     # 4) Load the data from npy file
     pmids, docs = utilities.process_data_from_npy(data_file)
@@ -40,12 +35,11 @@ def run(best_params, args, tuning=False, save_model=False):
     similarity_df = utilities.get_similarity_scores(ground_truth, embeddings_df)
     logging.info(f"RELISH {dataset_type} Cosine Similarity Matrix Generated.")
 
-     # 7) If the dataset type is "Test", then save the dataframes to a file each
-    if dataset_type=='Test':
-        embeddings_file = f"output_{args.classes}/embeddings/test_embeddings_{args.classes}.pkl"
-        similarity_file = f"output_{args.classes}/evaluation/test_cosine_similarity_{args.classes}.tsv"
-        utilities.save_embeddings_to_pickle(embeddings_df, embeddings_file)
-        utilities.save_similarity_to_tsv(similarity_df, similarity_file)
+    # 7) Save the dataframes to a file each
+    embeddings_file = f"output_{args.classes}/embeddings/test_embeddings_{args.classes}.pkl"
+    similarity_file = f"output_{args.classes}/evaluation/test_cosine_similarity_{args.classes}.tsv"
+    utilities.save_embeddings_to_pickle(embeddings_df, embeddings_file)
+    utilities.save_similarity_to_tsv(similarity_df, similarity_file)
 
     # 8) Save the model in the given path if specified
     if save_model:
